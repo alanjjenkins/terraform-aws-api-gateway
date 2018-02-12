9c1d9ef94f9cdeb1c17045cbae5980c0960baac3
Alan Jenkins <alan.jenkins@claranet.uk>
2018-02-12 20:40:06 +0000

    Initial commit.

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "rest_api_id" {
+  value = "${aws_api_gateway_rest_api.rest_api.id}"
+}
+
+output "rest_api_root_resource_id" {
+  value = "${aws_api_gateway_rest_api.rest_api.root_resource_id}"
+}
+
+output "domain_name" {
+  value = "${aws_api_gateway_domain_name.domain.domain_name}"
+}
+
+output "cloudfront_domain_name" {
+  value = "${aws_api_gateway_domain_name.domain.cloudfront_domain_name}"
+}
+
+output "cloudfront_zone_id" {
+  value = "${aws_api_gateway_domain_name.domain.cloudfront_zone_id}"
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "api_name" {}
+variable "api_description" {}
+variable "stage_name" {}
+variable "domain_name" {}
+variable "base_path" {}
+variable "acm_certificate_arn" {}
+variable "deployment_id" {}
